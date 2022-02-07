985f72b170d1135ee1a19ffd15692b6e352afef4
AnnaTsukiyo <annett.sav4enkko@gmail.com>
2022-02-07 13:36:19 +0200

    done

--- src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
+++ src/main/java/com/epam/rd/autotasks/validations/ColorCodeValidation.java
@@ -6,11 +6,11 @@ import java.util.regex.Pattern;
 
 public class ColorCodeValidation {
     public static boolean validateColorCode(String color) {
+        if (color == null || color.isEmpty() || color.isBlank())
+            return false;
         String expression = "^#([A-Fa-f0-9]{6}|[A-Fa-f0-9]{3})$";
         Pattern pattern = Pattern.compile(expression, Pattern.CASE_INSENSITIVE);
         Matcher matcher = pattern.matcher(color);
-        if (color == null || color.isEmpty() || color.isBlank())
-            return false;
         return matcher.matches();
     }
 
